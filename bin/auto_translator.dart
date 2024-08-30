1796968ece94c0adad2e619cfc1df9efecf28905
theLee3 <46857988+theLee3@users.noreply.github.com>
2024-08-30 14:54:07 -0400

    Update version number.

--- bin/auto_translator.dart
+++ bin/auto_translator.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:auto_translator/src/main.dart';
 
 void main(List<String> arguments) {
-  const version = '2.3.2';
+  const version = '2.3.3';
   stdout.writeln('auto_translator v$version');
   stdout.writeln('═════════════════════');
   runWithArguments(arguments).then((_) => exit(0)).catchError((error) {
